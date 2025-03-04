3c8d952ea5230774097e7e81df95b86a8229474a
Tuan Anh <tuananh030305@gmail.com>
2025-03-04 23:43:08 +0700

    ArrayOperations

---
+++ lab1/src/ArrayOperations.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new scanner(System.in);
+
+        System.out.print("nhap so phan tu cua mang : ");
+        int n = scanner.nextInt();
+    }
+}
