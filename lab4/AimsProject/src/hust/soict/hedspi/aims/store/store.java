d5fad214a7124dafdb0cfe1218b74d7af3356eb0
AH_NQ <tuananh030305@gmail.com>
2025-05-02 01:01:55 +0700

    lab 04

---
+++ lab4/AimsProject/src/hust/soict/hedspi/aims/store/store.java
@@ -0,0 +1,5 @@
+package AimsProject.src.hust.soict.hedspi.aims.store;
+
+public class store {
+    
+}
---
+++ lab4/GUIProject/src/hust/soict/hedspi/aims/media/Track.java
@@ -0,0 +1,36 @@
+package hust.soict.hedspi.aims.media;
+
+public class Track implements Playable {
+    private String title;
+    private int length;
+
+    public Track(String title, int length) {
+        this.title = title;
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public void play() {
+        if (length > 0) {
+            System.out.println("Playing Track: " + title);
+            System.out.println("Track length: " + length);
+        } else {
+            System.out.println("Cannot play this track.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Track)) return false;
+        Track other = (Track) obj;
+        return this.title.equalsIgnoreCase(other.title) && this.length == other.length;
+    }
+}
